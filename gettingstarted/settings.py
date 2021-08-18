06afaff4876ff7f2bd8f14d5fd48024d2b2fdb69
Charan Sai Dasari <charansaidasari@gmail.com>
2021-08-18 23:22:59 +0530

    aba huc

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-MEDIA_URL = '/media/tata.png/'
+MEDIA_URL = '/media/favicon.ico/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
