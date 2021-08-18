108ce219bacbc6a4fc596d185bb7347bb6a3906d
Charan Sai Dasari <charansaidasari@gmail.com>
2021-08-18 11:43:02 +0530

    nbcaia

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['apcakare.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['apcakare.herokuapp.com','127.0.0.1','still-brushlands-83823.herokuapp']
 
 
 # Application definition
